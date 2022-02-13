14946fc4faefb898232c7d68e521b4c9fda964e4
Fabian Greimel <fabgrei@gmail.com>
2022-02-13 23:10:07 +0100

    remove header from notebooks, remove youtube-id from one notebook

--- notebooks/week12/how_to_collaborate_on_software.jl
+++ notebooks/week12/how_to_collaborate_on_software.jl
@@ -12,55 +12,6 @@ begin
 end
 
 # ╔═╡ 10ebd434-adba-11eb-048f-2d084049d48f
-html"""
-<div style="
-position: absolute;
-width: calc(100% - 30px);
-border: 50vw solid #282936;
-border-top: 500px solid #282936;
-border-bottom: none;
-box-sizing: content-box;
-left: calc(-50vw + 15px);
-top: -500px;
-height: 500px;
-pointer-events: none;
-"></div>
-
-<div style="
-height: 500px;
-width: 100%;
-background: #282936;
-color: #fff;
-padding-top: 68px;
-">
-<span style="
-font-family: Vollkorn, serif;
-font-weight: 700;
-font-feature-settings: 'lnum', 'pnum';
-"> <p style="
-font-size: 1.5rem;
-opacity: .8;
-"><em>Section 3.5</em></p>
-<p style="text-align: center; font-size: 2rem;">
-<em> How to collaborate on software </em>
-</p>
-
-<p style="
-font-size: 1.5rem;
-text-align: center;
-opacity: .8;
-"><em>Lecture Video</em></p>
-<div style="display: flex; justify-content: center;">
-<div  notthestyle="position: relative; right: 0; top: 0; z-index: 300;">
-<iframe src="https://www.youtube.com/embed/7N9Vvc8amGM" width=400 height=250  frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
-</div>
-</div>
-
-<style>
-body {
-overflow-x: hidden;
-}
-</style>"""
 
 # ╔═╡ ef4eea24-bc1c-43be-b9b6-d073ac2433cf
 md"""
